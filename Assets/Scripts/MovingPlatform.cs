0c130c733344293ce38554db617d0574a86e2353
Rehma Razzak <32600945+rrazzak@users.noreply.github.com>
2020-02-01 17:01:13 -0500

    Add files via upload
    This script is for moving platforms. Parameters are Move(speed- how fast), Dir Path(these take values of 1 or -1.), Dis(distance, how far) and Axis(you can enter in either x, y, or z for this. Because our game is a 2D platformer x and y are used.)

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+//script for moving platforms
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float move = 3;
+    private Vector3 startPos;
+    public float dirPath;
+    public float dis;
+    public string axis;
+    // Use this for initialization
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    //this method will help determine what direction the platforms move in, whether its the x y or z axis
+    void FixedUpdate()
+    {
+        if (axis == "x")
+        {
+            transform.position = new Vector3(dirPath * Mathf.PingPong(Time.time * move, dis) + startPos.x, transform.position.y, transform.position.z);
+        }
+
+        else if (axis == "y")
+        {
+            transform.position = new Vector3(transform.position.x, dirPath * Mathf.PingPong(Time.time * move, dis) + startPos.y, transform.position.z);
+        }
+
+        else if (axis == "z")
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, dirPath * Mathf.PingPong(Time.time * move, dis) + startPos.z);
+        }
+
+    }
+}
